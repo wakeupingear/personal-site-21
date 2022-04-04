b1bc5152b2a7000bdb385bb1dc1bf5e9ee7d0c8c
Will Farhat <35930179+willf668@users.noreply.github.com>
2022-04-04 01:41:47 -0700

    meatball

--- src/components/pages/Jam.tsx
+++ src/components/pages/Jam.tsx
@@ -3,6 +3,7 @@ import React, { ReactElement } from 'react'
 import ProjectInfo from '../ProjectInfo'
 
 import i1 from "../../assets/jam/companion.png"
+import i2 from "../../assets/jam/meatball.png"
 
 interface Props {
 
@@ -14,11 +15,35 @@ export default function Jam(props: Props): ReactElement {
         <div className="pageHolder" id="JamPage">
             <div className="pageColumn jamColumn">
                 <ProjectInfo
+                    id="meatball"
+                    color="#ECD9BA"
+                    title='The Meatball in the Machine'
+                    text={[
+                        "Fling your meaty friend to freedom in this majestic puzzle game for people of all ages! Utilizing a variety of tools, guide your meatball through numerous unique and delightful levels. Watch as your bond with this meaty boi grows, in an experience that's sure to live on forever in your dreams!",
+                        "Made in 48 hours for the MEGA April Fools Jam",
+                    ]}
+                    images={[i2]}
+                    slideWidth={10}
+                    nested={true}
+                    collaborators={[
+                        ['Levi Pinkert', 'Gameplay Programming', 'https://levipinkert.wixsite.com/portfolio'],
+                        ['Charlie Heatherly', 'Art'],
+                        ['Will Farhat', 'Tech Art + Programming'],
+                        ['William McCullen', 'Music + Audio'],
+                        ['Drake Sorkhab', 'Level Design'],
+                        ['Nick Doherty', 'Level Design'],
+                    ]}
+                    links={[
+                        ["Play Now!", "https://williammccullen.itch.io/the-meatball-in-the-machine"],
+                    ]}
+                    date="April 2022"
+                />
+                <ProjectInfo
                     id="writus"
                     color="#343434"
                     title='Writus'
                     text={[
-                        "Play as a heavenly security guard who must stop rogue souls from entering the wrong afterlife! Make use of Dual Window Technology™ to see the true intent of every wandering mortal...",
+                        "A rythmic word scrambler on a 3x3 grid.",
                         "Made in 36 hours for HackSC 2022. Won 1st place in Entertainment",
                     ]}
                     images={["https://willfarhat.com/writus/squareLogo2.PNG"]}
